c77f0a2ee3db6b7c98440640def6e69402be6b22
Jeremy Wong <jermspeaks@gmail.com>
2015-03-07 22:07:59 -0800

    commented out stuff

--- js/dhpang-controllers.js
+++ js/dhpang-controllers.js
@@ -309,202 +309,202 @@ dhp.controller('PatientController', function($scope, DHPService) {
 });
 
 /* The observations controller retrieves observations and sets the $scope model for the observations view. */
-dhp.controller('ObservationsController', function($scope, DHPService) {
-	$scope.observationsShow = false;
-	$scope.showNext = false;
-	$scope.showPrevious = false;
+// dhp.controller('ObservationsController', function($scope, DHPService) {
+// 	$scope.observationsShow = false;
+// 	$scope.showNext = false;
+// 	$scope.showPrevious = false;
 
-	$scope.$on('Successful patient', function() {
-		$scope.observationsShow = true;
-		DHPService.observations();
-	});
-	$scope.$on('Successful logout', function() {
-		$scope.observationsShow = false;
-	});
+// 	$scope.$on('Successful patient', function() {
+// 		$scope.observationsShow = true;
+// 		DHPService.observations();
+// 	});
+// 	$scope.$on('Successful logout', function() {
+// 		$scope.observationsShow = false;
+// 	});
 
-	$scope.getObservations = function() {
-		DHPService.observations();
-	};
+// 	$scope.getObservations = function() {
+// 		DHPService.observations();
+// 	};
 
-	$scope.getObservationsNext = function() {
-		DHPService.observationsNext();
-	}
+// 	$scope.getObservationsNext = function() {
+// 		DHPService.observationsNext();
+// 	}
 
-	$scope.getObservationsPrevious = function() {
-		DHPService.observationsPrevious();
-	}
+// 	$scope.getObservationsPrevious = function() {
+// 		DHPService.observationsPrevious();
+// 	}
 
-	$scope.$on('Successful observations', function() {
-		var resultsCount;
-		var obsTable = [];
-		var k = 0;
-		var links = [];
-		var linkObj = {};
-		var prevFlag;
-		var nextFlag;
+// 	$scope.$on('Successful observations', function() {
+// 		var resultsCount;
+// 		var obsTable = [];
+// 		var k = 0;
+// 		var links = [];
+// 		var linkObj = {};
+// 		var prevFlag;
+// 		var nextFlag;
 
-		$.each(DHPService.getObservationsData(), function(key, val) {
-			if (key === "totalResults") {
-				$scope.total_results = "Total Records: " + val;
-				resultsCount = val;
-			}
-			if (key === "link") {
-				for (j = 0; j < val.length; j++) {
-					linkObj['id'] = j;
-					linkObj['href'] = val[j]['href'];
-					linkObj['rel'] = val[j]['rel'];
-					if (linkObj['rel'] === 'next') {
-						nextFlag = true;
-						DHPService.setObservationsNext(linkObj['href']);
-					}
-					if (linkObj['rel'] === 'previous') {
-						prevFlag = true;
-						DHPService.setObservationsPrev(linkObj['href']);
-					}
-					links.push(linkObj);
-				}
-			}
-			if (key === "entry") {
-				for (i = 0; i < val.length; i++) {
-					var obs = {};
-					var vals = [];
+// 		$.each(DHPService.getObservationsData(), function(key, val) {
+// 			if (key === "totalResults") {
+// 				$scope.total_results = "Total Records: " + val;
+// 				resultsCount = val;
+// 			}
+// 			if (key === "link") {
+// 				for (j = 0; j < val.length; j++) {
+// 					linkObj['id'] = j;
+// 					linkObj['href'] = val[j]['href'];
+// 					linkObj['rel'] = val[j]['rel'];
+// 					if (linkObj['rel'] === 'next') {
+// 						nextFlag = true;
+// 						DHPService.setObservationsNext(linkObj['href']);
+// 					}
+// 					if (linkObj['rel'] === 'previous') {
+// 						prevFlag = true;
+// 						DHPService.setObservationsPrev(linkObj['href']);
+// 					}
+// 					links.push(linkObj);
+// 				}
+// 			}
+// 			if (key === "entry") {
+// 				for (i = 0; i < val.length; i++) {
+// 					var obs = {};
+// 					var vals = [];
 
-					obs['id'] = k++;
+// 					obs['id'] = k++;
 
-					vals = dhpUtils.getObjects(val[i], 'display'); // getObjects(TestObj, 'id'); // Returns an array of matching objects
-					for (p = 0; p < vals.length; p++) {
-						if (vals[p]['system'] === "http://loinc.org") {
-							obs['display'] = vals[p]['display'];
-						} else {
-							obs['display'] = vals[p]['display'];
-						}
-					}
-					vals = dhpUtils.getObjects(val[i], 'value');
-					for (p = 0; p < vals.length; p++) {
-						obs['value'] = vals[p]['value'];
-					}
-					vals = dhpUtils.getObjects(val[i], 'units');
-					for (p = 0; p < vals.length; p++) {
-						obs['units'] = vals[p]['units'];
-					}
-					vals = dhpUtils.getObjects(val[i], 'status');
-					for (p = 0; p < vals.length; p++) {
-						obs['status'] = vals[p]['status'];
-					}
-					vals = dhpUtils.getObjects(val[i], 'reliability');
-					for (p = 0; p < vals.length; p++) {
-						obs['reliability'] = vals[p]['reliability'];
-					}
-					vals = dhpUtils.getObjects(val[i], 'start');;
-					if (vals.length > 0) {
-						for (p = 0; p < vals.length; p++) {
-							obs['datetime'] = vals[p]['start'];
-						}
-					}
-					vals = dhpUtils.getObjects(val[i], 'end');
-					if (vals.length > 0) {
-						for (p = 0; p < vals.length; p++) {
-							obs['datetime'] = obs['datetime'] + 'to' + vals[p]['end'];
-						}
-					}
-					vals = dhpUtils.getObjects(val[i], 'appliesDateTime');
-					if (vals.length > 0) {
-						for (p = 0; p < vals.length; p++) {
-							obs['datetime'] = vals[p]['appliesDateTime'];
-						}
-					}
-					obsTable.push(obs);
-				}
-			}
-		});
-		$scope.showNext = nextFlag;
-		$scope.showPrevious = prevFlag;
-		$scope.obsTable = obsTable;
-	});
-});
+// 					vals = dhpUtils.getObjects(val[i], 'display'); // getObjects(TestObj, 'id'); // Returns an array of matching objects
+// 					for (p = 0; p < vals.length; p++) {
+// 						if (vals[p]['system'] === "http://loinc.org") {
+// 							obs['display'] = vals[p]['display'];
+// 						} else {
+// 							obs['display'] = vals[p]['display'];
+// 						}
+// 					}
+// 					vals = dhpUtils.getObjects(val[i], 'value');
+// 					for (p = 0; p < vals.length; p++) {
+// 						obs['value'] = vals[p]['value'];
+// 					}
+// 					vals = dhpUtils.getObjects(val[i], 'units');
+// 					for (p = 0; p < vals.length; p++) {
+// 						obs['units'] = vals[p]['units'];
+// 					}
+// 					vals = dhpUtils.getObjects(val[i], 'status');
+// 					for (p = 0; p < vals.length; p++) {
+// 						obs['status'] = vals[p]['status'];
+// 					}
+// 					vals = dhpUtils.getObjects(val[i], 'reliability');
+// 					for (p = 0; p < vals.length; p++) {
+// 						obs['reliability'] = vals[p]['reliability'];
+// 					}
+// 					vals = dhpUtils.getObjects(val[i], 'start');;
+// 					if (vals.length > 0) {
+// 						for (p = 0; p < vals.length; p++) {
+// 							obs['datetime'] = vals[p]['start'];
+// 						}
+// 					}
+// 					vals = dhpUtils.getObjects(val[i], 'end');
+// 					if (vals.length > 0) {
+// 						for (p = 0; p < vals.length; p++) {
+// 							obs['datetime'] = obs['datetime'] + 'to' + vals[p]['end'];
+// 						}
+// 					}
+// 					vals = dhpUtils.getObjects(val[i], 'appliesDateTime');
+// 					if (vals.length > 0) {
+// 						for (p = 0; p < vals.length; p++) {
+// 							obs['datetime'] = vals[p]['appliesDateTime'];
+// 						}
+// 					}
+// 					obsTable.push(obs);
+// 				}
+// 			}
+// 		});
+// 		$scope.showNext = nextFlag;
+// 		$scope.showPrevious = prevFlag;
+// 		$scope.obsTable = obsTable;
+// 	});
+// });
 
 /* The graph controller retrieves graph data and sets the $scope model for the graph view. */
-dhp.controller('GraphController', function($scope, $window, $timeout, DHPService) {
-	$scope.graphShow = false;
+// dhp.controller('GraphController', function($scope, $window, $timeout, DHPService) {
+// 	$scope.graphShow = false;
 
-	$scope.getGraph = function() {
-		DHPService.observationsGlucose();
-	};
+// 	$scope.getGraph = function() {
+// 		DHPService.observationsGlucose();
+// 	};
 
-	$scope.$on('Successful logout', function() {
+// 	$scope.$on('Successful logout', function() {
 
-		$timeout(function() {
-			$window.location.reload();
-		}, 2000);
+// 		$timeout(function() {
+// 			$window.location.reload();
+// 		}, 2000);
 
-		$scope.graphShow = false;
+// 		$scope.graphShow = false;
 
 
-	});
+// 	});
 
-	$scope.$on('Successful patient', function() {
-		$scope.graphShow = true;
-	});
+// 	$scope.$on('Successful patient', function() {
+// 		$scope.graphShow = true;
+// 	});
 
-	$scope.$on('glucoseObsSuccess', function() {
+// 	$scope.$on('glucoseObsSuccess', function() {
 
-		var resultsCount;
-		var obsList = [];
-		var k = 0;
-		var links = [];
-		var linkObj = {};
-		var times = [];
-		var readings = [];
+// 		var resultsCount;
+// 		var obsList = [];
+// 		var k = 0;
+// 		var links = [];
+// 		var linkObj = {};
+// 		var times = [];
+// 		var readings = [];
 
-		$.each(DHPService.getGlucoseObservationsData(), function(key, val) {
-			if (key === "totalResults") {
-				resultsCount = val;
-			}
-			/* The code below can be expanded if one wanted to page the data and/or retrieve additional results. */
-			if (key === "link") {
-				for (j = 0; j < val.length; j++) {
-					if (val[j]['rel'] === 'next') {
-						linkObj['id'] = j;
-						linkObj['href'] = val[j]['href'];
-						linkObj['rel'] = val[j]['rel'];
+// 		$.each(DHPService.getGlucoseObservationsData(), function(key, val) {
+// 			if (key === "totalResults") {
+// 				resultsCount = val;
+// 			}
+// 			/* The code below can be expanded if one wanted to page the data and/or retrieve additional results. */
+// 			if (key === "link") {
+// 				for (j = 0; j < val.length; j++) {
+// 					if (val[j]['rel'] === 'next') {
+// 						linkObj['id'] = j;
+// 						linkObj['href'] = val[j]['href'];
+// 						linkObj['rel'] = val[j]['rel'];
 
-						links.push(linkObj);
-					}
-				}
-			}
-			if (key === "entry") {
-				for (i = 0; i < val.length; i++) {
+// 						links.push(linkObj);
+// 					}
+// 				}
+// 			}
+// 			if (key === "entry") {
+// 				for (i = 0; i < val.length; i++) {
 
-					var obs = {};
-					var vals = [];
+// 					var obs = {};
+// 					var vals = [];
 
-					obs['id'] = k++;
+// 					obs['id'] = k++;
 
-					vals = dhpUtils.getObjects(val[i], 'value');
-					for (p = 0; p < vals.length; p++) {
-						obs['value'] = vals[p]['value'];
-						readings.push(vals[p]['value']);
-					}
-					vals = dhpUtils.getObjects(val[i], 'appliesDateTime');
-					if (vals.length > 0) {
-						for (p = 0; p < vals.length; p++) {
-							obs['datetime'] = vals[p]['appliesDateTime'];
-							times.push(vals[p]['appliesDateTime']);
-						}
-					}
-					obsList.push(obs);
-				}
-			}
-		});
+// 					vals = dhpUtils.getObjects(val[i], 'value');
+// 					for (p = 0; p < vals.length; p++) {
+// 						obs['value'] = vals[p]['value'];
+// 						readings.push(vals[p]['value']);
+// 					}
+// 					vals = dhpUtils.getObjects(val[i], 'appliesDateTime');
+// 					if (vals.length > 0) {
+// 						for (p = 0; p < vals.length; p++) {
+// 							obs['datetime'] = vals[p]['appliesDateTime'];
+// 							times.push(vals[p]['appliesDateTime']);
+// 						}
+// 					}
+// 					obsList.push(obs);
+// 				}
+// 			}
+// 		});
 
-		$scope.labels = times;
-		if (resultsCount > 0) {
-			$scope.series = ['Glucose Observations'];
-		} else {
-			$scope.nograph = "There are no glucose observations for graphing."
-		}
-		$scope.data = [
-			readings
-		];
-	});
-});
+// 		$scope.labels = times;
+// 		if (resultsCount > 0) {
+// 			$scope.series = ['Glucose Observations'];
+// 		} else {
+// 			$scope.nograph = "There are no glucose observations for graphing."
+// 		}
+// 		$scope.data = [
+// 			readings
+// 		];
+// 	});
+// });
